058bbec06f4081e77665d7f32b94c782142a1598
Roberto <rr_xwcw@hotmail.com>
2023-12-02 14:04:57 -0500

    Day 2 Part 1

--- AdventOfCode/AdventOfCode.py
+++ AdventOfCode/AdventOfCode.py
@@ -1,7 +1,7 @@
 import sys
 import re
 
-def main():
+def day_1():
     # Create a variable to read files
     filename = "Files/Day_1.txt"
     total = 0
@@ -22,5 +22,45 @@ def main():
             
     print(total)
 
+def day_2():
+    p1_total = 0
+    red_limit = 12
+    green_limit = 13
+    blue_limit = 14
+
+    # Create a variable to read files
+    filename = "Files/Day_2.txt"
+
+    # Open file and split into "rows" (lines)
+    with open(filename) as f:
+        lines = f.readlines()
+        for game in lines:
+            # Split the line into game number and sets
+            game_number, sets_str = game.split(":", 1)
+            game_number = game_number.replace("Game ", "").strip()
+            sets = sets_str.strip().split(";")
+
+            game_valid = True
+
+            for set_ in sets:
+                set_ = set_.replace("\n", "").replace(":", "")
+                colors = set_.split(",")
+                for color in colors:
+                    red, green, blue = "0", "0", "0"
+                    if "red" in color:
+                        red = color.replace("red", "").replace(" ", "")
+                    elif "green" in color:
+                        green = color.replace("green", "").replace(" ", "")
+                    elif "blue" in color:
+                        blue = color.replace("blue", "").replace(" ", "")
+
+                    if int(red) > red_limit or int(blue) > blue_limit or int(green) > green_limit:
+                        game_valid = False
+
+            if game_valid:
+                p1_total += int(game_number)
+
+    print(p1_total)
+
 if __name__ == '__main__':
-    main()
+    day_2()
