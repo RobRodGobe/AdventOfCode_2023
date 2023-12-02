7c114ac505b57ead6416f8a176a9ad60d190b5a5
Roberto <rr_xwcw@hotmail.com>
2023-12-02 18:26:35 -0500

    Day 2 part 2

--- AdventOfCode/AdventOfCode.py
+++ AdventOfCode/AdventOfCode.py
@@ -24,6 +24,7 @@ def day_1():
 
 def day_2():
     p1_total = 0
+    p2_total = 0
     red_limit = 12
     green_limit = 13
     blue_limit = 14
@@ -41,10 +42,13 @@ def day_2():
             sets = sets_str.strip().split(";")
 
             game_valid = True
+                    
+            red_min, green_min, blue_min = 0, 0, 0
 
             for set_ in sets:
                 set_ = set_.replace("\n", "").replace(":", "")
-                colors = set_.split(",")
+                colors = set_.split(",")                
+
                 for color in colors:
                     red, green, blue = "0", "0", "0"
                     if "red" in color:
@@ -57,10 +61,27 @@ def day_2():
                     if int(red) > red_limit or int(blue) > blue_limit or int(green) > green_limit:
                         game_valid = False
 
+                    if int(red) > red_min:
+                        red_min = int(red)
+                    if int(blue) > blue_min:
+                        blue_min = int(blue)
+                    if int(green) > green_min:
+                        green_min = int(green)
+
+            if(red_min == 0):
+                red_min = 1
+            if(blue_min == 0):
+                blue_min = 1
+            if(green_min == 0):
+                green_min = 1
+                
+
+            p2_total += red_min * blue_min * green_min
             if game_valid:
                 p1_total += int(game_number)
 
     print(p1_total)
+    print(p2_total)
 
 if __name__ == '__main__':
     day_2()
