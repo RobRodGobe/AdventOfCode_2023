82ca70866fddcad5327c8b7aa13e39643cf47aa2
Roberto <rr_xwcw@hotmail.com>
2023-12-16 21:31:49 -0500

    Day 16

--- AdventOfCode/AdventOfCode.py
+++ AdventOfCode/AdventOfCode.py
@@ -907,5 +907,114 @@ def day_15():
 
     print (p1, p2)
 
+    
+def day_16():
+    filename = "Files/Day_16.txt"
+    p1 = 0
+    p2 = 0
+
+    with open(filename, "r") as file:
+        puzzle_input = file.read()
+
+    def part1(puzzle_input):
+
+        grid = [list(r) for r in puzzle_input.split('\n')]
+        m, n = len(grid), len(grid[0])
+        visited = set()
+        energized = set()
+        queue = set([(0, 0, 'right')])   
+        while queue:
+            x, y, direction = queue.pop()
+            energized.add((x, y))
+            tile = grid[x][y]
+
+            if y < n-1 and (x, y+1, 'right') not in visited and (
+                    (direction == 'right' and tile in '.-') or 
+                    (direction == 'up' and tile in '/-') or
+                    (direction == 'down' and tile in '\\-')):
+                queue.add((x, y+1, 'right'))
+                visited.add((x, y+1, 'right'))
+
+            if x > 0 and (x-1, y, 'up') not in visited and (
+                    (direction == 'up' and tile in '.|') or 
+                    (direction == 'right' and tile in '/|') or
+                    (direction == 'left' and tile in '\\|')):
+                queue.add((x-1, y, 'up'))
+                visited.add((x-1, y, 'up'))
+
+            if y > 0 and (x, y-1, 'left') not in visited and (
+                    (direction == 'left' and tile in '.-') or 
+                    (direction == 'up' and tile in '\\-') or
+                    (direction == 'down' and tile in '/-')):
+                queue.add((x, y-1, 'left'))
+                visited.add((x, y-1, 'left'))
+
+            if x < m-1 and (x+1, y, 'down') not in visited and (
+                    (direction == 'down' and tile in '.|') or 
+                    (direction == 'right' and tile in '\\|') or
+                    (direction == 'left' and tile in '/|')):
+                queue.add((x+1, y, 'down'))     
+                visited.add((x+1, y, 'down'))
+
+        return len(energized)
+
+
+    def part2(puzzle_input):
+    
+        grid = [list(r) for r in puzzle_input.split('\n')]
+        m, n = len(grid), len(grid[0])
+        initial = ({(x, 0, 'right') for x in range(m)} |
+                   {(x, n-1, 'left') for x in range(m)} |
+                   {(m-1, y, 'up') for y in range(n)} |
+                   {(0, y, 'down') for y in range(n)})
+    
+        best = 0
+        for i in initial:
+            visited = set()
+            energized = set()
+            queue = set([i])   
+            while queue:
+                x, y, direction = queue.pop()
+                energized.add((x, y))
+                tile = grid[x][y]
+
+                if y < n-1 and (x, y+1, 'right') not in visited and (
+                        (direction == 'right' and tile in '.-') or 
+                        (direction == 'up' and tile in '/-') or
+                        (direction == 'down' and tile in '\\-')):
+                    queue.add((x, y+1, 'right'))
+                    visited.add((x, y+1, 'right'))
+
+                if x > 0 and (x-1, y, 'up') not in visited and (
+                        (direction == 'up' and tile in '.|') or 
+                        (direction == 'right' and tile in '/|') or
+                        (direction == 'left' and tile in '\\|')):
+                    queue.add((x-1, y, 'up'))
+                    visited.add((x-1, y, 'up'))
+
+                if y > 0 and (x, y-1, 'left') not in visited and (
+                        (direction == 'left' and tile in '.-') or 
+                        (direction == 'up' and tile in '\\-') or
+                        (direction == 'down' and tile in '/-')):
+                    queue.add((x, y-1, 'left'))
+                    visited.add((x, y-1, 'left'))
+
+                if x < m-1 and (x+1, y, 'down') not in visited and (
+                        (direction == 'down' and tile in '.|') or 
+                        (direction == 'right' and tile in '\\|') or
+                        (direction == 'left' and tile in '/|')):
+                    queue.add((x+1, y, 'down'))     
+                    visited.add((x+1, y, 'down'))
+
+            best = max(best, len(energized))
+
+        return best
+
+
+    p1 = part1(puzzle_input)
+    p2 = part2(puzzle_input)
+
+    print (p1, p2)
+
 if __name__ == '__main__':
-    day_15()
+    day_16()
