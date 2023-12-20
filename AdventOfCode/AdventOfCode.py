8f06a38eafe3299e339841090a86915d07806396
Roberto <rr_xwcw@hotmail.com>
2023-12-19 21:20:55 -0500

    Day 18

--- AdventOfCode/AdventOfCode.py
+++ AdventOfCode/AdventOfCode.py
@@ -1047,5 +1047,80 @@ def day_17():
 
     print (p1, p2)
 
+def day_18():
+    filename = "Files/Day_18.txt"
+    p1 = 0
+    p2 = 0
+
+
+    mm = list(map(lambda x: x.strip().split(' '), open(filename, "r").readlines()))
+    dd = {'R': (0, 1), 'D': (1, 0), 'L': (0, -1), 'U': (-1, 0)}  
+
+    def f(mm):
+        p = []
+        x = 0
+        y = 0
+        for m in mm:
+            (d, l) = m
+            if d == 'D':
+                p.append((y, x, 'D'))
+                p.append((y + l, x, 'U'))
+            elif d == 'U': 
+                p.append((y, x, 'U'))
+                p.append((y - l, x, 'D'))
+            y = y + (dd[d][0] * l)
+            x = x + (dd[d][1] * l)
+        p.sort(key=(lambda x: (1000 if x[2] == 'U' else 0) + x[0] * 1000000 + x[1]))
+
+        l = []
+        q = 0
+        t = 0
+        while q < len(p):
+            ny = p[q][0]
+            i = 0
+            while i < len(l):
+                if ny > y + 1 and l[i+1] > l[i]:
+                    t += ((l[i+1] - l[i])+ 1) * (ny - y - 1)
+                i += 2
+            y = ny
+            a = l.copy()
+            while q < len(p) and p[q][0] == ny:
+                if p[q][2] == 'D':
+                    l.append(p[q][1])
+                else:
+                    l.remove(p[q][1])
+                q += 1
+            l.sort()
+            b = l.copy()
+            i = j = 0
+            tt = t
+            while (i < len(a) or j < len(b)):
+                if i < len(a) and j < len(b):
+                    if a[i] > b[j+1]:
+                        t += ((b[j+1] - b[j]) + 1)
+                        j += 2
+                    elif b[j] > a[i+1]:
+                        t += ((a[i+1] - a[i]) + 1)
+                        i += 2
+                    elif a[i+1] > b[j+1]:
+                        a[i] = min(a[i], b[j])
+                        j += 2
+                    else:
+                        b[j] = min(a[i], b[j])
+                        i += 2
+                elif i < len(a):
+                    t += ((a[i+1] - a[i]) + 1)
+                    i += 2
+                else:
+                    t += ((b[j+1] - b[j]) + 1)
+                    j += 2
+        return t
+
+    p1 = f(list([(m[0], int(m[1])) for m in mm]))
+    DD = { 0: 'R', 1: 'D', 2: 'L', 3: 'U' }
+    p2 = f(list([(DD[int(m[2][7])], int(m[2][2:7], 16)) for m in mm]))
+
+    print (p1, p2)
+
 if __name__ == '__main__':
-    day_17()
+    day_18()
