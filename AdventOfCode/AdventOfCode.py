e7d66c96d4e30ecc4d32633feb5802438781d18c
Roberto <rr_xwcw@hotmail.com>
2023-12-25 14:17:10 -0500

    Day 25

--- AdventOfCode/AdventOfCode.py
+++ AdventOfCode/AdventOfCode.py
@@ -6,7 +6,7 @@ import re
 from functools import reduce, lru_cache
 from collections import defaultdict, deque
 from typing import List, Tuple, Dict
-import math, time
+import math, time, random
 import heapq
 from functools import reduce
 import operator
@@ -1609,5 +1609,89 @@ def day_24(filename="Files/Day_24.txt"):
 
     print(p1, p2)
 
+def day_25(filename="Files/Day_25.txt"):
+    p1, p2 = 0, 0
+
+    with open(filename, "r") as f:
+        arr = [line.strip() for line in f]
+
+    graph = {}
+
+    for line in arr:
+        a, links = line.split(": ")
+        links = links.split(" ")
+        for b in links:
+            graph[a] = graph.get(a, []) + [b]
+            graph[b] = graph.get(b, []) + [a]
+
+
+    def get_comp_size(root, banned):
+        """
+        assuming we have pruned off the correct edges, this returns the size of on of the two components
+        """
+        nodes = [root]
+        seen = {root}
+        while nodes:
+            new_nodes = []
+            for node in nodes:
+                for neighbour in graph[node]:
+                    if neighbour in seen or (node, neighbour) in banned or (neighbour, node) in banned:
+                        continue
+                    seen.add(neighbour)
+                    new_nodes.append(neighbour)
+            nodes = new_nodes
+        return len(seen)
+
+
+    def get_path(start, end):
+        """
+        returns the shortest path between start and end
+        The solution doesn't NEED the shortest path (in fact it might be better random) but each cycle is quicker if we do
+        """
+        prev = {start: start}
+        nodes = [start]
+        seen = {start}
+        while nodes:
+            new_nodes = []
+            for node in nodes:
+                for neighbour in graph[node]:
+                    if neighbour in seen:
+                        continue
+                    seen.add(neighbour)
+                    prev[neighbour] = node
+                    new_nodes.append(neighbour)
+            nodes = new_nodes
+
+        if prev.get(end) is None:
+            return None
+
+        path = []
+        node = end
+        while node != start:
+            path.append(node)
+            node = prev[node]
+        path.append(start)
+        return path[::-1]
+
+
+    uses = {}
+    for _ in range(100): # higher range means more likely to be correct
+        a, b = random.sample(list(graph.keys()), 2)
+        path = get_path(a, b)
+        for i in range(len(path) - 1):
+            edge = tuple(sorted([path[i], path[i + 1]]))
+            uses[edge] = uses.get(edge, 0) + 1
+
+
+    # the three most used edges will be the three we need to cut
+    s_uses = sorted(uses.items(), key=lambda x: x[1], reverse=True)
+
+    banned = [p[0] for p in s_uses[:3]]
+
+    # get the size of each sets
+    s1, s2 = get_comp_size(banned[0][0], banned), get_comp_size(banned[0][1], banned)
+    p1 = (s1*s2)
+    print(p1, p2)
+
 if __name__ == "__main__":
-    day_24()
+    day_25()
